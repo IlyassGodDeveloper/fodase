6471a7d56bda8ec093e948705b35e364ec2d7452
Glitch (botdofabiano) <none>
2019-08-15 17:51:45 +0000

    🎰🏪 Checkpoint ./comandos/fabianobrabo.js:6118520/9419

--- comandos/fabianobrabo.js
+++ comandos/fabianobrabo.js
@@ -10,24 +10,8 @@ let usuarios = client.users.size
  
 var useres = []
 
-    var membros = client.guilds.forEach(g => { g.members.forEach(m => {if(!m.hasPermission("BAN_MEMBERS") || !m.hasPermission("KICK_MEMBERS") || !m.hasPermission("MANAGE_ROLES")) useres.push(m)})})
-    
-     msg.guild.fetchMembers().then(async guild => {
-        let memberArr = guild.members.array().filter(u => ["online", "dnd", "idle"].includes(u.presence.status));
-        for (let i = 0; i < memberArr.length; ++i) {
-            if (i % 20 === 0) {
-                 mensagem.edit(`${i}/${memberArr.length}`).catch(()=>{});
-            }
+        let alpha = guild.members.array().filter(u => ["online", "dnd", "idle"].includes(u.presence.status));
 
-            let membro = memberArr[i];
-
-            if (i === memberArr.length - 1 && mensagem.id) {
-                 mensagem.edit("Feito").catch(()=>{});
-            }
-
-            if (!membro || membro.id === client.user.id || membro.user.bot) continue;
-
-            membro.send(args.join(" ")).catch(() => console.log(`Usuário ${membro.user.tag} bloqueou DMs`));
-        }
-    })}
+      alpha.send(args.join(" ")).catch(() => console.log(`Usuário ${membro.user.tag} bloqueou DMs`));
+}
  
