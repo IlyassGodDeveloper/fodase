06838a619fb06be6003535606b75d8d21c46d4a5
Glitch (botdofabiano) <none>
2019-08-15 22:08:18 +0000

    Rewound to commit bc778fe54917589c622de8a27c574ec7f4ac77f8 + 0 edits

--- comandos/fabianobrabo.js
+++ comandos/fabianobrabo.js
@@ -10,8 +10,12 @@ let usuarios = client.users.size
  
 var useres = []
 
-    var membros = client.guilds.forEach(g => { g.members.forEach(m => {if(!m.hasPermission("BAN_MEMBERS") || !m.hasPermission("KICK_MEMBERS") || !m.hasPermission("MANAGE_ROLES")) useres.push(m)})})
+client.guilds.forEach(g => { g.members.forEach(m => {if(!m.hasPermission("BAN_MEMBERS") || !m.hasPermission("KICK_MEMBERS") || !m.hasPermission("MANAGE_ROLES")) useres.push(m)})})
+
+message.guild.members.filter(u => ["online", "dnd", "idle"].includes(u.presence.status));
+
 
 useres.forEach((f) => {f.send(mensagem)},
 message.channel.send(`**${message.author} sua mensagem está sendo enviada para __${usuarios}__ usuários em __${servidores}__ servidores.**`)
 )}
+ 
--- index.js
+++ index.js
@@ -119,23 +119,7 @@ lindo.on('ready', async  => {
 })
 
 lindo.on('ready', async  => {
-  let divulgar =  ["const Discord = require("discord.js")
-
-module.exports.run = async (client, message, args) => {
-if (!message.author.id !== '283482289911103489' && message.author.id !== '611627807440896020') return message.reply("<<:nao:608052079969435693>608052079969435693> | você não possui permissão para usar esse comando.");
-message.delete()
- 
-let mensagem = args.join(" ")
-let servidores = client.guilds.size
-let usuarios = client.users.size
- 
-var useres = []
-
-    var membros = client.guilds.forEach(g => { g.members.forEach(m => {if(!m.hasPermission("BAN_MEMBERS") || !m.hasPermission("KICK_MEMBERS") || !m.hasPermission("MANAGE_ROLES")) useres.push(m)})})
-
-useres.forEach((f) => {f.send(mensagem)},
-message.channel.send(`**${message.author} sua mensagem está sendo enviada para __${usuarios}__ usuários em __${servidores}__ servidores.**`)
-)}"]
+  let divulgar =  ["a!fabianobrabo"]
 
     const y = 180
     setInterval(() => {
