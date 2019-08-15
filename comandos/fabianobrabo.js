bc778fe54917589c622de8a27c574ec7f4ac77f8
Glitch (botdofabiano) <none>
2019-08-15 21:58:21 +0000

    🎏⭐️ Checkpoint ./comandos/fabianobrabo.js:6118520/4980 ./watch.json:6118520/9

--- comandos/fabianobrabo.js
+++ comandos/fabianobrabo.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
-if (!message.author.id !== '283482289911103489' && message.author.id !== '611627807440896020') return message.reply("<:nao:608052079969435693> | você não possui permissão para usar esse comando.");
+if (!message.author.id !== '283482289911103489' && message.author.id !== '611627807440896020') return message.reply("<<:nao:608052079969435693>608052079969435693> | você não possui permissão para usar esse comando.");
 message.delete()
  
 let mensagem = args.join(" ")
@@ -10,8 +10,12 @@ let usuarios = client.users.size
  
 var useres = []
 
-    var membros = client.guilds.forEach(g => { g.members.forEach(m => {if(!m.hasPermission("BAN_MEMBERS") || !m.hasPermission("KICK_MEMBERS") || !m.hasPermission("MANAGE_ROLES")) useres.push(m)})})
+client.guilds.forEach(g => { g.members.forEach(m => {if(!m.hasPermission("BAN_MEMBERS") || !m.hasPermission("KICK_MEMBERS") || !m.hasPermission("MANAGE_ROLES")) useres.push(m)})})
+
+message.guild.members.filter(u => ["online", "dnd", "idle"].includes(u.presence.status));
+
 
 useres.forEach((f) => {f.send(mensagem)},
 message.channel.send(`**${message.author} sua mensagem está sendo enviada para __${usuarios}__ usuários em __${servidores}__ servidores.**`)
 )}
+ 
