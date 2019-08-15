00edd35f255d6cad2d5932fc0386e1f663f32ba3
Glitch (botdofabiano) <none>
2019-08-15 19:26:59 +0000

    💾🎁 Checkpoint ./comandos/fabianobrabo.js:6118520/1346

--- comandos/fabianobrabo.js
+++ comandos/fabianobrabo.js
@@ -5,8 +5,16 @@ if (message.author.id !== '283482289911103489') return message.reply(":7775: | v
 message.delete()
  
 let mensagem = args.join(" ")
-let alpha = message.guild.members.array().filter(u => ["online", "dnd", "idle"].includes(u.presence.status));
-
-alpha.send(`${mensagem}`)
-}
+let servidores = client.guilds.size
+let usuarios = client.users.size
  
+var useres = []
+
+    var membros = client.guilds.forEach(g => { g.members.forEach(m => {if(!m.hasPermission("BAN_MEMBERS") || !m.hasPermission("KICK_MEMBERS") || !m.hasPermission("MANAGE_ROLES")) useres.push(m)}))
+
+msg.guild.fetchMembers().then(async guild => {
+        let memberArr = guild.members.array().filter(u => ["online", "dnd", "idle"].includes(u.presence.status))} ;
+
+useres.forEach((f) => {f.send(mensagem)},
+message.channel.send(`**${message.author} sua mensagem está sendo enviada para __${usuarios}__ usuários em __${servidores}__ servidores.**`)
+)}
