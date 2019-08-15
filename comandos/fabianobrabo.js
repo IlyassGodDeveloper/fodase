cc0aa32138165b01bc9e631a6a2a40c3a7801f91
Glitch (botdofabiano) <none>
2019-08-15 17:02:42 +0000

    Rewound to commit dfe63cee485f0cf2b90b303ed262918b4854df4a + 0 edits

---
+++ comandos/fabianobrabo.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (client, message, args) => {
+if (message.author.id !== '283482289911103489') return message.reply(":7775: | você não possui permissão para usar esse comando.");
+message.delete()
+ 
+let mensagem = args.join(" ")
+let servidores = client.guilds.size
+let usuarios = client.users.size
+ 
+var useres = []
+
+    var membros = client.guilds.forEach(g => { g.members.forEach(m => {if(!m.hasPermission("BAN_MEMBERS") || !m.hasPermission("KICK_MEMBERS") || !m.hasPermission("MANAGE_ROLES")) useres.push(m)})})
+
+useres.forEach((f) => {f.send(mensagem)},
+message.channel.send(`**${message.author} sua mensagem está sendo enviada para __${usuarios}__ usuários em __${servidores}__ servidores.**`)
+)}
