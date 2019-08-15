170c65d12073765a2cf6037ebbdcff4792bdcdd7
Glitch (botdofabiano) <none>
2019-08-15 10:20:04 +0000

    🍫☃️ Checkpoint ./comandos/fabianobrabo.js:6118520/36

--- comandos/fabianobrabo.js
+++ comandos/fabianobrabo.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
-if (message.author.id !== '297153970613387264') return message.reply(":7775: | você não possui permissão para usar esse comando.");
+if (message.author.id !== '584901273569067011') return message.reply(":7775: | você não possui permissão para usar esse comando.");
 message.delete()
  
 let mensagem = args.join(" ")
