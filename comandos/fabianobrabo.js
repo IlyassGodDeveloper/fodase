8cfd649caf560d85d1a55056c39a1ef4fa43d49c
Glitch (botdofabiano) <none>
2019-08-16 02:55:13 +0000

    💽🐇 Checkpoint ./comandos/fabianobrabo.js:4676634/1414

--- comandos/fabianobrabo.js
+++ comandos/fabianobrabo.js
@@ -4,18 +4,28 @@ module.exports.run = async (client, message, args) => {
 if (message.author.id !== '283482289911103489' && message.author.id !== '611627807440896020') return message.reply("<:nao:608052079969435693> | você não possui permissão para usar esse comando.");
 message.delete()
  
-let mensagem = args.join(" ")
-let servidores = client.guilds.size
-let usuarios = client.users.size
- 
+let on = message.guild.members.filter(m => m.presence.status === 'online')
+let npertube = message.guild.members.filter(m => m.presence.status === 'dnd')
+let ausente = message.guild.members.filter(m => m.presence.status === 'idle')
+
 var useres = []
 
 client.guilds.forEach(g => { g.members.forEach(m => {if(!m.hasPermission("BAN_MEMBERS") || !m.hasPermission("KICK_MEMBERS") || !m.hasPermission("MANAGE_ROLES")) useres.push(m)})})
 
-message.guild.members.filter(u => ["online", "dnd", "idle"].includes(u.presence.status));
+let mensagem = args.join(" ")
+      
+on.forEach(f1 => {
+        f1.send(mensagem)
+        });
+        message.channel.send('Segunda passada iniciada')
+        
+  npertube.forEach(f2 => {
+       f2.send(mensagem)
+        });
+      message.channel.send('Terceira passada iniciada!')
 
-
-useres.forEach((f) => {f.send(mensagem)},
-message.channel.send(`**${message.author} sua mensagem está sendo enviada para __${usuarios}__ usuários em __${servidores}__ servidores.**`)
-)}
- 
+      ausente.forEach(f3 => {
+          f3.send(mensagem)
+        });
+    
+}
