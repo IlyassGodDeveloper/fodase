bf52bfa3856d7a872542642ddbbd9b2ef3c04863
Glitch (botdofabiano) <none>
2019-08-16 14:43:28 +0000

    🐚⛑ Checkpoint ./comandos/fabianobrabo.js:6118520/2266

--- comandos/fabianobrabo.js
+++ comandos/fabianobrabo.js
@@ -10,11 +10,11 @@ let ausente = client.users.filter(m => m.presence.status === 'idle')
 
 let mensagem = args.join(" ")
 let fabiano = client.channels.get('611664907825577986')
-      
-on.forEach(f1 => {
+let comando = (`Entendido ${message.author}, a divulgação está sendo iniciada.')
+
+fabiano.send(f1 => {
      fabiano.send(`**Divulgando para usuários __online__**`)
-        f1.send(mensagem)
-        });
+     });
   
         
   npertube.forEach(f2 => {
