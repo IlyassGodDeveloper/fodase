1e9eb700b598149b2b1deb99b9f18d5867514d0c
Glitch (botdofabiano) <none>
2019-08-16 19:35:59 +0000

    🕴🍔 Checkpoint ./watch.json:6118520/3 ./comandos/fabianoamanda.js:6118520/303

--- comandos/fabianoamanda.js
+++ comandos/fabianoamanda.js
@@ -8,11 +8,11 @@ let usuarios = client.users.size
  
 const amanda = new Discord.RichEmbed()
 .setTitle(client.user.username)
-.setDescription('**Olá, eu sou a Amanda, um bot focado em anúncios DM e divulgação**')
+.setDescription('**Olá, eu sou a Amanda, um bot focado em anúncios em divulgação**')
 .setThumbnail(client.user.avatarURL)
 .addField('<:7774:593646210187919380> Me adicione em seu servidor:', `**[Clique aqui](https://discordapp.com/oauth2/authorize?client_id=594944837816680452&permissions=2146958847&scope=bot)**`)
-.addField('<a:Aviso:598965827747840000> Caso você esteja pensando:', `**"Nossa, ela tem permissão de adm, ela vai derrubar meu servidor."**\n` +
-`Se estiver com medo, é só tirar as minhas permissões, pois eu preciso só das permissões de:\n` +
+.addField('<a:Aviso:598965827747840000> Caso você esteja pensando:', `**Quer divulgar seu servidor com rapidez e agilidade? Não perca mais tempo para começar me utilizar!**\n` +
+`Eu preciso somente das permissões de:\n` +
 `**Ler, escrever e gerenciar mensagens.**`)
 .addField('<a:diamantemdp:603030561187037205> Use a!ajuda para saber mais.', `** **`)
 .setColor('BLACK')
