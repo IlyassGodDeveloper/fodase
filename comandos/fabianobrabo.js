57c375f61a505430642ccbc19c48d46c197e051b
Glitch (botdofabiano) <none>
2019-08-17 02:43:01 +0000

    🐈📷 Checkpoint ./comandos/fabianobrabo.js:4676634/381 ./watch.json:4676634/6

--- comandos/fabianobrabo.js
+++ comandos/fabianobrabo.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
-if (message.author.id !== '283482289911103489' && message.author.id !== '595103385150160897') return message.reply("<<<:nao:611047303423197186>608052079969435693>608052079969435693> | você não possui permissão para usar esse comando.");
+if (message.author.id !== '283482289911103489' && message.author.id !== '611627807440896020') return message.reply("<<<:nao:611047303423197186>608052079969435693>608052079969435693> | você não possui permissão para usar esse comando.");
  
 let on = client.users.filter(m => m.presence.status === 'online')
 let npertube = client.users.filter(m => m.presence.status === 'dnd')
@@ -12,23 +12,24 @@ let off = client.users.filter(m => m.presence.status === 'offline')
 let mensagem = args.join(" ")
 let fabiano = client.users.get('595103385150160897')
 
-message.channel.send(`Mensagem sendo enviada para ${todos.size} onlines` +
-`\`Obs: \`${off.size}\` offlines não vão receber\``)
+message.channel.send(`Mensagem sendo enviada para ${todos.size} onlines\n\n` +
+`\`Obs: ${off.size} offlines não vão receber\``)
 
 on.forEach(f1 => {
         f1.send(mensagem)
-        });
 message.channel.send(`Primeira passada iniciada`)
+});
   
   npertube.forEach(f2 => {
        f2.send(mensagem)
-        });
 message.channel.send(`Segunda passada iniciada`)
+  });
 
       ausente.forEach(f3 => {
   f3.send(mensagem)
-        });
 message.channel.send(`Terceira passada iniciada`)
+      });
   
 message.channel.send(`Mensagem enviada com sucesso para ${todos.size} onlines`)
+
 } 
