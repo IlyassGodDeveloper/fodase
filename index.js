ec6775690db36454f39fa92080c6142830036472
Glitch (botdofabiano) <none>
2019-08-17 05:52:07 +0000

    📇😜 Checkpoint ./index.js:6118520/607 ./watch.json:6118520/1

--- index.js
+++ index.js
@@ -116,9 +116,9 @@ lindo.on('ready', async  => {
 })
 
 lindo.on('ready', async  => {
-  let div =  ["a!fabianobrabo _**`🔥 Entra aqui neném, me dá uma forcinha, vai. ❤️`**_\n\nLink: https://discord.gg/uqWTGcA\n\nhttps://cdn.discordapp.com/attachments/595731795190874112/595738403698376721/fgc.gif"] 
+  let div =  ["a!fabianobrab _**🙈  Oi pessoa linda, venho te convidar para o servidor da família mais quente do Discord.**_\n\n⠀⠀⠀⠀⠀🎊  Eventos todos os dias\n\n⠀⠀⠀⠀🔱 Vagas na Staff\n\n⠀⠀⠀⠀⠀🔞  Conteúdo +18\n\n⠀⠀⠀⠀💕  Garotas\n\n⠀⠀⠀📌 E muuuito mais!\n\n⠀⠀🎈  __**Estamos te esperando!**__ 🎈\n\n⠀⠀➡️ ⠀https://discord.gg/uqWTGcA\n\nhttps://cdn.discordapp.com/attachments/595731795190874112/595738403698376721/fgc.gif"] 
  
-  const y = 30
+  const y = 1
   setInterval(() => {
 
  lindo.channels.get("612157427072368662").send(div)
