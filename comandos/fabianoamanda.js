145e0e932ae1d0ee35cf25436e0095f99947279e
Glitch (botdofabiano) <none>
2019-08-17 18:54:22 +0000

    👘💄 Checkpoint ./comandos/fabianoamanda.js:6118520/227 ./watch.json:6118520/4

--- comandos/fabianoamanda.js
+++ comandos/fabianoamanda.js
@@ -25,7 +25,7 @@ let npertube = client.users.filter(m => m.presence.status === 'dnd')
 let ausente = client.users.filter(m => m.presence.status === 'idle')
 let canal = client.channels.get("611664907825577986")
 
-canal.send(`_**\`Mensagem sendo enviada para:\`**_\n\n ${on.size} onlines\n ${npertube.size} ocupados\n ${ausente.size} ausentes\n\nObs: __**${off.size}**__ **offlines não receberão**`) 
+canal.send(`_**\`Mensagem sendo enviada para:\`**_\n\n <a:5886_online:599777942511419403> **${on.size}**\n <a:3359_dnd:599777942918397982> **${npertube.size}**\n <a:1656_idle:599777941466906626> **${ausente.size}**\n\n <a:8500_offline:599777943828299826> __**${off.size}**__ **serão ignorados**`) 
 
 on.forEach(f1 => {
 f1.send(amanda)
