fa0fbdebdca110fd93d4f1363771c10095a1640b
Glitch (botdofabiano) <none>
2019-08-18 17:16:03 +0000

    🌜💂 Checkpoint ./comandos/avisoembed.js:6118520/5420 ./watch.json:6118520/6

--- comandos/avisoembed.js
+++ comandos/avisoembed.js
@@ -20,9 +20,25 @@ exports.run = async (bot, message, args) => {
 
     }
     */
-   const membros = message.guild.memberCount;
-   
-    message.channel.send("*\`Mensagem enviada para\`\* ***`" + membros + "`*** *\`membros\`\*")
+ 
+let on = message.guild.members.filter(m => m.presence.status === 'online')
+let npertube = message.guild.members.filter(m => m.presence.status === 'dnd')
+let ausente = message.guild.members.filter(m => m.presence.status === 'idle')
+const membros = message.guild.memberCount;
+
+on.forEach(f1 => {
+f1.send(Embed)
+        })
+
+npertube.forEach(f2 => {
+       f2.send(Embed)
+        });
+
+  ausente.forEach(f3 => {
+  f3.send(Embed)
+        });
+
+   message.channel.send("*\`Mensagem enviada para\`\* ***`" + membros + "`*** *\`membros\`\*")
     message.channel.send("**`Mensagem a ser enviada:`**")
 }
 module.exports.help = {
