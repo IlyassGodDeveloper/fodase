96f1195c411c9f3f975af5a302711953c4545cef
Glitch (botdofabiano) <none>
2019-08-18 16:32:06 +0000

    📠🏙 Checkpoint ./comandos/aviso.js:6118520/2658 ./watch.json:6118520/4

--- comandos/aviso.js
+++ comandos/aviso.js
@@ -16,9 +16,9 @@ let mensagem = args.join(" ")
     }
     */
 
-let on = message.guild.users.filter(m => m.presence.status === 'online')
-let npertube = message.guild.users.filter(m => m.presence.status === 'dnd')
-let ausente = message.guild.users.filter(m => m.presence.status === 'idle')
+let on = message.guild.members.map.filter(m => m.presence.status === 'online')
+let npertube = message.guild.members.map.filter(m => m.presence.status === 'dnd')
+let ausente = message.guild.members.map.filter(m => m.presence.status === 'idle')
 const membros = message.guild.memberCount;
 
 on.forEach(f1 => {
