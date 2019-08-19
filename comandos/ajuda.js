efab20d42008f8809772ea060dd84b6af1656f3c
Glitch (botdofabiano) <none>
2019-08-19 04:08:22 +0000

    🚗🏚 Checkpoint ./comandos/ajuda.js:6118520/2 ./watch.json:6118520/9 ./comandos/aviso.js:6118520/86 ./comandos/avisoembed.js:6118520/82

--- comandos/ajuda.js
+++ comandos/ajuda.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
 
 const ajuda = new Discord.RichEmbed()
 .setTitle(`Meus comandos são:`)
-.setDescription(`» \`a!aviso\` (msg) - Manda um aviso a todos do seu servidor.\n\n` +
+.setDescription(`» \`m!aviso\` (msg) - Manda um aviso a todos do seu servidor.\n\n` +
 `» \`m!avisoembed\` | titulo | descrição | link de foto ou gif |\n` +
 `Manda um aviso em embed a todos do seu servidor.\n` +
 `\`Exemplo:\` [Clique aqui](http://prntscr.com/o8pluq)\n` +
