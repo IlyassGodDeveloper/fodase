7b2172c7ecd4e2e3db2a84242e16fdb95da52398
Glitch (mariana) <none>
2019-08-23 15:40:38 +0000

    🐵🏁 Checkpoint ./comandos/aviso.js:6118520/45

--- comandos/aviso.js
+++ comandos/aviso.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("ADMINISTRATOR") && message.author.id !== '283482289911103489') return message.reply("<:7775:593645935280783431> | você não possui permissão para usar esse comando.")
+  if(message.author.id = '
     message.delete()
 let mensagem = args.join(" ")
     /* try{
