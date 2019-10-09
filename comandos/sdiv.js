5c0dec622cc4d8e82f3d3c4899f11e70b5f2d6b8
Glitch (sofiabot) <none>
2019-10-09 17:39:58 +0000

    🐂✌️ Checkpoint ./index.js:5159062/23 ./config.json:5159062/160 ./comandos/sdiv.js:5159062/76 ./comandos/sbot.js:5159062/78 ./watch.json:5159062/3

--- comandos/sdiv.js
+++ comandos/sdiv.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
   if (
-    message.author.id !== "624003960663179264" &&
-    message.author.id !== "620099629622493194"
+    message.author.id !== "568234099093798923" &&
+    message.author.id !== "609790051286777927"
   )
     return message.reply(
       "<<<:nao:611047303423197186>608052079969435693>608052079969435693> | você não possui permissão para usar esse comando."
