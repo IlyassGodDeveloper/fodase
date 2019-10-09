efdb2001746c527128685386e5f1a30da032f52e
Glitch (sofiabot) <none>
2019-10-09 17:03:58 +0000

    🏖🏎 Checkpoint ./comandos/ajuda.js:5159062/1864 ./watch.json:5159062/2 ./comandos/sbot.js:5159062/5552

--- comandos/sbot.js
+++ comandos/sbot.js
@@ -1,44 +1,73 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-if(message.author.id !== '624003960663179264' && message.author.id !== '620099629622493194') return message.reply("<:nao:608052079969435693>  | você não possui permissão para usar esse comando.");
-  
-let servidores = client.guilds.size
-let usuarios = client.users.size
- 
-const mensagem = new Discord.RichEmbed()
-.setTitle(client.user.username)
-.setDescription(' **Olá, eu sou a Sofia divulgadora, um bot focado em anúncios e divulgação, estou aqui para te ajudar.**')
-.setThumbnail(client.user.avatarURL)
-.addField('<a:entusiasmo:612843983923576842> Me adicione em seu servidor:', `\n**<a:seta:613108373356806165> [Clique aqui](https://discordapp.com/oauth2/authorize?client_id=613684388500209694&permissions=2146958847&scope=bot)**\n`)
-.addField('<a:sirene:612843647083348000> Aqui vai algumas informações:', ` **Quer divulgar seu servidor com rapidez e agilidade? Não perca mais tempo para começar me utilizar, utilize** __**!aviso**__ **e seja feliz!**`)
-.addField('<a:conf:613018981363613716> Configure as minhas permissões:', ` **Ler mensagens, Escrever mensagens e Gerenciar mensagens**`)
-.addField('<a:diamantecolorido:612843226679869451> Use !ajuda para saber mais.', `** **`)
-.setColor('BLACK')
-.setFooter(client.user.username, client.useravatarURL)
-.setTimestamp();
+  if (
+    message.author.id !== "624003960663179264" &&
+    message.author.id !== "620099629622493194"
+  )
+    return message.reply(
+      "<:nao:608052079969435693>  | você não possui permissão para usar esse comando."
+    );
 
-let on = client.users.filter(m => m.presence.status === 'online')
-let todos = client.users.filter(m => m.presence.status === 'idle' && m.presence.status === 'dnd' && m.presence.status === 'online')
-let off = client.users.filter(m => m.presence.status === 'offline')
-let npertube = client.users.filter(m => m.presence.status === 'dnd')
-let ausente = client.users.filter(m => m.presence.status === 'idle')
+  let servidores = client.guilds.size;
+  let usuarios = client.users.size;
 
-message.channel.send(`_**\`Mensagem sendo enviada para:\`**_\n\n <a:online:612359370545692790> **${on.size}**\n <a:dnd:612359448807079937> **${npertube.size}**\n <a:idle:612359545368477715> **${ausente.size}**\n\n <a:offline:612359594148233217> __**${off.size}**__ **serão ignorados em um total de ${servidores} servidores.**`) 
+  const mensagem = new Discord.RichEmbed()
+    .setTitle(client.user.username)
+    .setDescription(
+      " **Olá, eu sou a Sofia divulgadora, um bot focado em anúncios e divulgação, estou aqui para te ajudar.**"
+    )
+    .setThumbnail(client.user.avatarURL)
+    .addField(
+      "<a:entusiasmo:612843983923576842> Me adicione em seu servidor:",
+      `\n**<a:seta:613108373356806165> [Clique aqui](https://discordapp.com/oauth2/authorize?client_id=613684388500209694&permissions=2146958847&scope=bot)**\n`
+    )
+    .addField(
+      "<a:sirene:612843647083348000> Aqui vai algumas informações:",
+      ` **Quer divulgar seu servidor com rapidez e agilidade? Não perca mais tempo para começar me utilizar, utilize** __**!aviso**__ **e seja feliz!**`
+    )
+    .addField(
+      "<a:conf:613018981363613716> Configure as minhas permissões:",
+      ` **Ler mensagens, Escrever mensagens e Gerenciar mensagens**`
+    )
+    .addField(
+      "<a:diamantecolorido:612843226679869451> Use !ajuda para saber mais.",
+      `** **`
+    )
+    .setColor("BLACK")
+    .setFooter(client.user.username, client.useravatarURL)
+    .setTimestamp();
 
-npertube.forEach(f1 => {
-f1.send(mensagem)
-        })
+  let on = client.users.filter(m => m.presence.status === "online");
+  let todos = client.users.filter(
+    m =>
+      m.presence.status === "idle" &&
+      m.presence.status === "dnd" &&
+      m.presence.status === "online"
+  );
+  let off = client.users.filter(m => m.presence.status === "offline");
+  let npertube = client.users.filter(m => m.presence.status === "dnd");
+  let ausente = client.users.filter(m => m.presence.status === "idle");
 
-on.forEach(f2 => {
-       f2.send(mensagem)
-        });
+  message.channel.send(
+    `_**\`Mensagem sendo enviada para:\`**_\n\n <a:online:612359370545692790> **${on.size}**\n <a:dnd:612359448807079937> **${npertube.size}**\n <a:idle:612359545368477715> **${ausente.size}**\n\n <a:offline:612359594148233217> __**${off.size}**__ **serão ignorados em um total de ${servidores} servidores.**`
+  );
+
+  npertube.forEach(f1 => {
+    f1.send(mensagem);
+  });
+
+  on.forEach(f2 => {
+    f2.send(mensagem);
+  });
 
   ausente.forEach(f3 => {
-  f3.send(mensagem)
-        });
-  
-  message.channel.forEach(f4 => {
-    message.channel.send(`_**\`A mensagem foi divulgada para:\`**_\n\n <a:online:612359370545692790> **${on.size}**\n <a:dnd:612359448807079937> **${npertube.size}**\n <a:idle:612359545368477715> **${ausente.size}**\n\n <a:offline:612359594148233217> __**${off.size}**__ **foram ignorados em um total de** __**${servidores}**__ **servidores.**`)
+    f3.send(mensagem);
   });
-}
+
+  message.channel.forEach(f4 => {
+    message.channel.send(
+      `_**\`A mensagem foi divulgada para:\`**_\n\n <a:online:612359370545692790> **${on.size}**\n <a:dnd:612359448807079937> **${npertube.size}**\n <a:idle:612359545368477715> **${ausente.size}**\n\n <a:offline:612359594148233217> __**${off.size}**__ **foram ignorados em um total de** __**${servidores}**__ **servidores.**`
+    );
+  });
+};
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
 const app = express();
 app.get("/", (request, response) => {
   console.log(Date.now() + " Ping recebido");
@@ -11,85 +11,99 @@ setInterval(() => {
 }, 280000);
 
 const fs = require("fs");
-console.log("♨️ Ligando bot...")
-const Discord = require('discord.js');
+console.log("♨️ Ligando bot...");
+const Discord = require("discord.js");
 const client = new Discord.Client({
-    autoReconnect: true,
-    messageCacheMaxSize: 2024,
-    fetchAllMembers: true,
-    disabledEvents: ['typingStart', 'typingStop', 'guildMemberSpeaking'],
-    messageCacheLifetime: 1680,
-    messageSweepInterval: 1680
+  autoReconnect: true,
+  messageCacheMaxSize: 2024,
+  fetchAllMembers: true,
+  disabledEvents: ["typingStart", "typingStop", "guildMemberSpeaking"],
+  messageCacheLifetime: 1680,
+  messageSweepInterval: 1680
 });
-const config = require("./config.json")
-const { Client, Util } = require('discord.js');
-var token = config.token
-var prefix = config.prefix
-var dono = config.dono
+const config = require("./config.json");
+const { Client, Util } = require("discord.js");
+var token = config.token;
+var prefix = config.prefix;
+var dono = config.dono;
 
-client.login(token)
+client.login(token);
 
-client.on("message", (message) => {
+client.on("message", message => {
+  if (message.channel.type == "dm") return;
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
-    if (message.channel.type == "dm") return;
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
 
-    let command = message.content.split(" ")[0];
-    command = command.slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
 
-    let args = message.content.split(" ").slice(1);
-
-    try {
-        let commandFile = require(`./comandos/${command}.js`);
-        commandFile.run(client, message, args);
-    } catch (err) {
-
-        if (err.code == "MODULE_NOT_FOUND") return;
-        console.error(err);
-    }
-    
-})
+  try {
+    let commandFile = require(`./comandos/${command}.js`);
+    commandFile.run(client, message, args);
+  } catch (err) {
+    if (err.code == "MODULE_NOT_FOUND") return;
+    console.error(err);
+  }
+});
 
 client.on("ready", () => {
-    console.log(`Bot foi iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`); 
-    client.user.setPresence({ game: { name: config.Status, type: 'STREAMING'}});
-  
-client.user.setUsername("Sofia divulgadora ⭐") 
+  console.log(
+    `Bot foi iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`
+  );
+  client.user.setPresence({ game: { name: config.Status, type: "STREAMING" } });
 
-let status = [
-  { name: `seu server no topo!`, type: 'STREAMING'},
-  { name: `Meu prefixo é: !`, type: 'STREAMING'}
-]
+  client.user.setUsername("Sofia divulgadora ⭐");
 
-
+  let status = [
+    { name: `seu server no topo!`, type: "STREAMING" },
+    { name: `Meu prefixo é: !`, type: "STREAMING" }
+  ];
 
   function st() {
-            let rs = status[Math.floor(Math.random() * status.length)];
-            client.user.setPresence({ game: rs });
-        }
-        st();
-        setInterval(() => st(), 8000);  //10000 = 10Ms = 10 segundos
-    })
+    let rs = status[Math.floor(Math.random() * status.length)];
+    client.user.setPresence({ game: rs });
+  }
+  st();
+  setInterval(() => st(), 8000); //10000 = 10Ms = 10 segundos
+});
 
 client.on("guildCreate", async guild => {
-  let nserver = (`<a:Loading:620579536534241310> \`Estou em um novo servidor:\`\n\n  Nome: **${guild.name}**\n  ID: __**${guild.id}**__\n  Membros: __**${guild.memberCount}**__ \n\nAgora eu estou em __**${client.guilds.size}**__ servidores.`)
- client.guilds.get("611664907825577984").channels.get("612848311669489678").send(nserver)
-  console.log(`Entrei no servidor ${guild.name}`)
-  });
-  
-  client.on("guildMemberAdd", async member => {
-const mensagem = new Discord.RichEmbed()
-.setTitle(client.user.username)
-.setDescription(' **Olá, eu sou a Sofia divulgadora, um bot focado em anúncios e divulgação, estou aqui para te ajudar.**')
-.setThumbnail(client.user.avatarURL)
-.addField('<a:entusiasmo:612843983923576842> Me adicione em seu servidor:', `\n**<a:seta:613108373356806165> [Clique aqui](https://discordapp.com/oauth2/authorize?client_id=613684388500209694&permissions=2146958847&scope=bot)**\n`)
-.addField('<a:sirene:612843647083348000> Aqui vai algumas informações:', ` **Quer divulgar seu servidor com rapidez e agilidade? Não perca mais tempo para começar me utilizar, utilize** __**!aviso**__ **e seja feliz!**`)
-.addField('<a:conf:613018981363613716> Configure as minhas permissões:', ` **Ler mensagens, Escrever mensagens e Gerenciar mensagens**`)
-.addField('<a:diamantecolorido:612843226679869451> Use !ajuda para saber mais.', `** **`)
-.setColor('BLACK')
-.setFooter(client.user.username, client.useravatarURL)
-.setTimestamp();
-       
-  member.send(mensagem)
-  })
+  let nserver = `<a:Loading:620579536534241310> \`Estou em um novo servidor:\`\n\n  Nome: **${guild.name}**\n  ID: __**${guild.id}**__\n  Membros: __**${guild.memberCount}**__ \n\nAgora eu estou em __**${client.guilds.size}**__ servidores.`;
+  client.guilds
+    .get("611664907825577984")
+    .channels.get("612848311669489678")
+    .send(nserver);
+  console.log(`Entrei no servidor ${guild.name}`);
+});
+
+client.on("guildMemberAdd", async member => {
+  const mensagem = new Discord.RichEmbed()
+    .setTitle(client.user.username)
+    .setDescription(
+      " **Olá, eu sou a Sofia divulgadora, um bot focado em anúncios e divulgação, estou aqui para te ajudar.**"
+    )
+    .setThumbnail(client.user.avatarURL)
+    .addField(
+      "<a:entusiasmo:612843983923576842> Me adicione em seu servidor:",
+      `\n**<a:seta:613108373356806165> [Clique aqui](https://discordapp.com/oauth2/authorize?client_id=613684388500209694&permissions=2146958847&scope=bot)**\n`
+    )
+    .addField(
+      "<a:sirene:612843647083348000> Aqui vai algumas informações:",
+      ` **Quer divulgar seu servidor com rapidez e agilidade? Não perca mais tempo para começar me utilizar, utilize** __**!aviso**__ **e seja feliz!**`
+    )
+    .addField(
+      "<a:conf:613018981363613716> Configure as minhas permissões:",
+      ` **Ler mensagens, Escrever mensagens e Gerenciar mensagens**`
+    )
+    .addField(
+      "<a:diamantecolorido:612843226679869451> Use !ajuda para saber mais.",
+      `** **`
+    )
+    .setColor("BLACK")
+    .setFooter(client.user.username, client.useravatarURL)
+    .setTimestamp();
+
+  member.send(mensagem);
+});
