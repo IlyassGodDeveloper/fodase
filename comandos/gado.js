ebc91e239c9818dac29c5813ec53d0faabff8331
Glitch (sophia-bot) <none>
2019-11-12 23:18:25 +0000

    👋🖖 Checkpoint ./comandos/gado.js:5159062/31 ./watch.json:5159062/2

--- comandos/gado.js
+++ comandos/gado.js
@@ -6,7 +6,7 @@ module.exports.run = (bot, message, args) => {
   if (message.author.id == "1") return message.reply("você esta na blacklist.");
 
   message.channel.send(
-    "<:gadokkkj:636322696103198761> Um gado foi detectado, meninas favor evacuar a área."
+    "Um gado foi detectado, meninas favor evacuar a área."
   );
   message.delete();
 };
