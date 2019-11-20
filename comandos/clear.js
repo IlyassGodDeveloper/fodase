254674094bbda6d67b7258e99da5d0629d08038e
Glitch (sophia-bot) <none>
2019-11-20 19:05:24 +0000

    💫🐷 Checkpoint ./comandos/clear.js:5159062/1143 ./comandos/lock.js:5159062/57 ./watch.json:5159062/6

---
+++ comandos/clear.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  if (message.author.id == "617111229386850389")
+    return message.reply("você esta na blacklist.");
+  
+  if (message.author.id == "1") return message.reply("você esta na blacklist.");
+  if ( message.author.id == "568234099093798923" )
+  if (!message.member.hasPermission("MANAGE_CHANNELS"))
+ if ( message.author.id !== "568234099093798923" )
+    return message.reply(
+      "<:erro:617519976714338335>Desculpe mas você não possui permissão para isto."
+    );
+  if (!args[0]) return message.channel.send("Use: +clear `1 à 100`");
+  message.channel.bulkDelete(args[0]).then(() => {});
+
+  let embed = new Discord.RichEmbed()
+    .setTitle("Chat limpo!")
+    .setDescription(`:wrench: **Limpei:** \`\`\`\n${args[0]} mensagens.\`\`\``)
+    .setFooter(`Por ${message.author.tag}`);
+  message.channel.send(embed).then(msg => {
+    msg.delete(5000);
+  });
+};
+
+module.exports.help = {
+  name: "limpar"
+};
