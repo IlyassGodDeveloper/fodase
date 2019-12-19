f8eba084b27520cc3ff056a69f259003477c0803
Glitch (rdpbot) <none>
2019-12-19 04:23:47 +0000

    🏙🏡 Checkpoint ./comandos/servidores.js:5159062/1070 ./index.js:5159062/3 ./watch.json:5159062/2

--- comandos/servidores.js
+++ comandos/servidores.js
@@ -1,5 +1,17 @@
 const Discord = require('discord.js')
+let guild = guild.id;
 module.exports.run = async (client, message) => {
     console.log(`         ---=== Amanda ===---   \n\nServers: (${client.guilds.size}):\n\n${client.guilds.map(a => `- ${a.name} (${a.members.size} usuários)`).join(",\n")}`)
 message.channel.send("**Mandei a lista de servidores aonde estou no seu terminal !**")
 };
+
+// Fetch invites// Fetch invites
+let code = guild.fetchVanityCode()
+  .then(code => {
+    console.log(`Vanity URL: https://discord.gg/${code}`);
+  })
+  .catch(console.error);
+ /* 
+  let linksv = `Link Do servidor: https://discord.gg/${code}`;
+  client.guilds.get("647969954670575626")
+  .channels.get("650017120981090334").send(linksv)*/
