52bf6e25260249c36b198ef455101ccdccd9042a
Glitch (bot-burguesa) <none>
2019-12-24 19:38:20 +0000

    🏄🛢 Checkpoint ./comandos/setavatar.js:5159062/197

--- comandos/setavatar.js
+++ comandos/setavatar.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 module.exports.run = (bot, message, args, discord) => {
-    if (message.author.id !== "568234099093798923" &&
-       message.author.id !== "559825881271369795" &&
-       message.author.id !== "634366099970064384") return message.reply(":x:FAIL, você não possui permissão!");
+    if (//message.author.id !== "568234099093798923" &&
+      // message.author.id !== "559825881271369795" &&
+       message.author.id !== `${config.dono}` && message.author.id !== `${config.dev}` message) return message.reply(":x:FAIL, você não possui permissão!");
   let avatar = args.join(" ")
     bot.user.setAvatar(avatar);
     message.channel.send(`Set my avatar to ${args.join(' ')}!`);
